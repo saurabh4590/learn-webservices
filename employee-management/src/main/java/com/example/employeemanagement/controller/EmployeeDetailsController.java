19b77095dc571acfc5697a505dccb907baff4aa2
Sourabh Sharma <saurabh4590@gmail.com>
2020-04-08 15:37:09 +0530

    CustomException for 404 status

--- employee-management/src/main/java/com/example/employeemanagement/controller/EmployeeDetailsController.java
+++ employee-management/src/main/java/com/example/employeemanagement/controller/EmployeeDetailsController.java
@@ -1,5 +1,7 @@
 package com.example.employeemanagement.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,10 +12,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.employeemanagement.entity.Employee;
+import com.example.employeemanagement.exception.UserException;
 import com.example.employeemanagement.service.DetailsService;
 
 @RestController
 public class EmployeeDetailsController {
+
+	Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	@Autowired
 	private DetailsService detailsService;
@@ -27,11 +32,14 @@ public class EmployeeDetailsController {
 	@GetMapping("/details/{id}")
 	public ResponseEntity<Employee> getAllEmployees(@PathVariable("id") int id) {
 		Employee employee = detailsService.getEmployeeByid(id);
-		return new ResponseEntity<Employee>(employee, HttpStatus.OK);
+		if (null != employee)
+			return new ResponseEntity<Employee>(employee, HttpStatus.OK);
+		else
+			throw new UserException("User doesn't exist");
 	}
 
 	/**
-	 * Create an Employee record
+	 * Creates an Employee record
 	 * 
 	 * @param employee
 	 * @return
@@ -41,4 +49,6 @@ public class EmployeeDetailsController {
 		Employee tempEmployee = detailsService.createEmployee(employee);
 		return new ResponseEntity<Employee>(tempEmployee, HttpStatus.CREATED);
 	}
+
+	// TBD - PUT and DELETE
 }
